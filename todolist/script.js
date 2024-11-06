500730e31ae9da0957a44d5c289314c94142cbab
Mr Alpha <mohamaderahbar@yahoo.com>
2024-11-06 18:39:16 +0330

    all completed!

--- todolist/script.js
+++ todolist/script.js
@@ -1,27 +1,43 @@
 /* 
-delete
-
+Coded By Mr-Alpha
+it can be better
+and i will do it soon :)
+mind that, its a training! 
 */
 
 // holders 
 const $ = document;
 const todos = $.getElementById("todos")
 const addTodo = $.getElementById("addTodo")
-let newTodo;
 
 addTodo.addEventListener("keydown", function (e) {
-    if (e.key === "Enter") {
-        newTodo = addTodo.value
+    if (e.key === "Enter" && addTodo.value !== "") {
+
         let newLi = $.createElement("li")
+
         let newSpan = $.createElement("span")
+        newSpan.innerText = addTodo.value
+
         let trash = $.createElement("i")
+        trash.classList.add("fa", "fa-trash-o", "delete")
 
         newLi.appendChild(newSpan)
         newLi.appendChild(trash)
-        console.log(newLi);
 
+        todos.appendChild(newLi)
+
+        todos.style.display = "block";
         addTodo.value = ""
-        // i class="fa fa-trash-o delete
+
+        trash.addEventListener("click", function (e) {
+            if (todos.children.length < 2) {
+                todos.style.display = "none";
+            } else {
+                trash.parentElement.remove();
+            }
+        })
 
     }
 })
+
+
