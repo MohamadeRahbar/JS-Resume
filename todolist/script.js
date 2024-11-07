2ebf840ddd4f7374cd8d763aab2e1b75d5a1f26f
Mr Alpha <mohamaderahbar@yahoo.com>
2024-11-07 16:33:44 +0330

    refactor to functional code

--- todolist/script.js
+++ todolist/script.js
@@ -11,33 +11,38 @@ const todos = $.getElementById("todos")
 const addTodo = $.getElementById("addTodo")
 
 addTodo.addEventListener("keydown", function (e) {
-    if (e.key === "Enter" && addTodo.value !== "") {
-
-        let newLi = $.createElement("li")
-
-        let newSpan = $.createElement("span")
-        newSpan.innerText = addTodo.value
-
-        let trash = $.createElement("i")
-        trash.classList.add("fa", "fa-trash-o", "delete")
-
-        newLi.appendChild(newSpan)
-        newLi.appendChild(trash)
-
-        todos.appendChild(newLi)
-
+    // remove to add blank spaces by trim()
+    let getTodo = e.target.value.trim()
+    if (e.key === "Enter" && getTodo !== "") {
+        newTodo(getTodo)
         todos.style.display = "block";
         addTodo.value = ""
-
-        trash.addEventListener("click", function (e) {
-            if (todos.children.length < 2) {
-                todos.style.display = "none";
-            } else {
-                trash.parentElement.remove();
-            }
-        })
-
     }
 })
 
+function newTodo(getTodo) {
+    // create row 
+    let newLi = $.createElement("li")
 
+    // create <span>
+    let newSpan = $.createElement("span")
+    newSpan.innerText = addTodo.value
+
+    // create <i>
+    let trash = $.createElement("i")
+    trash.classList.add("fa", "fa-trash-o", "delete")
+
+    newLi.appendChild(newSpan)
+    newLi.appendChild(trash)
+    // functionality of trash 
+    trash.addEventListener("click", function () {
+        if (todos.children.length < 2) {
+            todos.style.display = "none";
+        } else {
+            trash.parentElement.remove();
+        }
+    })
+
+    todos.appendChild(newLi)
+
+}
