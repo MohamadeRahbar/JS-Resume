6b82155720a4af06d6b44b2b40eabdc872cda4de
mmrah <mohamaderahbar@yahoo.com>
2024-11-17 00:44:18 +0330

    minor changes

--- Modal/script.js
+++ Modal/script.js
@@ -1,19 +1,30 @@
 const $ = document;
 // holders 
-const modal = $.getElementsByClassName("modal - origin")
+const modal = $.querySelector(".modal-origin")
+const modalBtn = $.getElementById("modal-btn")
+const closeModal = $.querySelector("#close-modal")
 
-function openModal() {
+
+modalBtn.addEventListener("click", function (e) {
+    console.log(e);
     // TODO
 
-    //if click, modal-origin d-block 
+    //if click, modal-origin d-block
 
     // add transform : translateY(0)
 
     // add filter blur to section
+})
 
-}
 
-function closeModal() {
+
+modal.addEventListener("keydown", function (param) {
+    console.log(param);
+})
+
+
+closeModal.addEventListener("click", function (param) {
+
     // TODO
     // if click, modal-origin d-none
-}
+})
