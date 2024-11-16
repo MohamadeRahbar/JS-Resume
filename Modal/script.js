b6668479271e995a66b652901c6f2b2ee90a8c80
mmrah <mohamaderahbar@yahoo.com>
2024-11-16 22:42:11 +0330

    add algorithm by comments, TODO

---
+++ Modal/script.js
@@ -0,0 +1,24 @@
+// holders 
+
+const $ = document;
+
+modal - btn
+
+function openModal() {
+    // TODO
+
+    //if click, modal-origin d-block 
+
+    // add transform : translateY(0)
+
+    // add filter blur to section
+
+}
+
+// display 
+modal - origin
+
+function closeModal() {
+    // TODO
+    // if click, modal-origin d-none
+}
