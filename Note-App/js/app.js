6982e2e91d2054b4868a13f018b43059279994b9
mmrah <mohamaderahbar@yahoo.com>
2024-11-25 16:36:40 +0330

    all done! now it works

--- Note-App/js/app.js
+++ Note-App/js/app.js
@@ -4,24 +4,50 @@ const $ = document;
 const UI = {
     input: $.getElementById("input-field"),
     notes: $.getElementById("listed"),
-    colors: $.getElementsByClassName("color-box")
+    colors: $.querySelectorAll(".color-box")
 }
 
 UI.input.addEventListener("keyup", addNote)
 
-
-// colors foreach >> add event click e.target change UI.input bg and bg == true, if UI.inputs e.key == enter && bg == true >> note bg == e bg color 
-
-// add btn
 function addNote(e) {
-    // TODO Add UI.input.value to notes 
     if (e.key === "Enter") {
-        let note = UI.input.value
-        console.log(note);
+        createNote()
     }
 }
 
-// erase btn 
-function clearDisplay() {
-    //TODO clear UI.input.value
+function createNote() {
+    if (UI.input.value !== '') {
+        let note = UI.input.value;
+        let newDiv = $.createElement("div");
+        newDiv.classList.add("card", "shadow-sm", "rounded");
+        newDiv.innerHTML = `<p class="card-text p-3">${note}</p>`
+        newDiv.style.backgroundColor = UI.input.style.backgroundColor;
+        UI.notes.appendChild(newDiv)
+        clearUI()
+        UI.input.style.backgroundColor = "#fff"
+        // remove when click on note
+        newDiv.addEventListener("click", function () {
+            newDiv.remove()
+        })
+
+
+
+    } else {
+        UI.input.classList.add("redAlert")
+        setTimeout(function () {
+            UI.input.classList.remove("redAlert")
+        }, 1500)
+    }
 }
+
+function clearUI() {
+    UI.input.value = ""
+}
+
+
+// get color
+UI.colors.forEach(function (colorBtn) {
+    colorBtn.addEventListener('click', function (e) {
+        UI.input.style.backgroundColor = e.target.style.backgroundColor
+    })
+})
