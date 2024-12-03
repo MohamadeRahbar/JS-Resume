be482ea6cf0e55e31b276398b171d35af7c524b1
mmrah <mohamaderahbar@yahoo.com>
2024-12-03 20:11:12 +0330

    add slider

--- cb-ui-temp-01/js/sctipt.js
+++ cb-ui-temp-01/js/sctipt.js
@@ -1,8 +1,6 @@
-// holders
+// onScroll Menu 
 const $ = document
 const navbar = document.querySelector('.a-navbar');
-
-// onScroll Menu 
 $.addEventListener('scroll', function () {
     console.log("scroll");
     if ($.documentElement.scrollTop > 0) {
@@ -12,6 +10,7 @@ $.addEventListener('scroll', function () {
     }
 });
 
+// menuger of nav
 $.addEventListener("DOMContentLoaded", function () {
     const hamburgerMenu = document.querySelector(".menuger");
     const menu = document.querySelector(".a-navbar .menu");
@@ -20,3 +19,65 @@ $.addEventListener("DOMContentLoaded", function () {
         menu.classList.toggle("active");
     });
 });
+
+
+// slider
+const slider = {
+    ui: $.querySelector('#a-slider'),
+    slides: $.querySelectorAll('#slide'),
+    prevBtn: $.querySelector('.prev-btn'),
+    nxtBtn: $.querySelector('.next-btn')
+}
+let currentIndex = 0;
+let isMouseInside = false;
+
+// // show specific slide
+// function showSlide(index) {
+//     slides.forEach((slide, i) => {
+//         slide.classList.remove('active', 'previous', 'next');
+//         if (i === index) {
+//             slide.classList.add('active');
+//         } else if (i === (index - 1 + slides.length) % slides.length) {
+//             slide.classList.add('previous');
+//         } else if (i === (index + 1) % slides.length) {
+//             slide.classList.add('next');
+//         }
+//     });
+// }
+
+// // click on prev btn
+// prevBtn.addEventListener('click', () => {
+//     currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+//     showSlide(currentIndex);
+// });
+
+// //  click on next btn
+// nextBtn.addEventListener('click', () => {
+//     currentIndex = (currentIndex + 1) % slides.length;
+//     showSlide(currentIndex);
+// });
+
+// // flag for keyboard event
+// slider.addEventListener('mouseenter', () => {
+//     isMouseOverSlider = true;
+// });
+
+// slider.addEventListener('mouseleave', () => {
+//     isMouseOverSlider = false;
+// });
+
+// // check flag and then keyboard event
+// document.addEventListener('keydown', (event) => {
+//     if (isMouseOverSlider) {
+//         if (event.key === 'ArrowRight') {
+//             currentIndex = (currentIndex + 1) % slides.length;
+//             showSlide(currentIndex);
+//         } else if (event.key === 'ArrowLeft') {
+//             currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+//             showSlide(currentIndex);
+//         }
+//     }
+// });
+
+// // show default slide
+// showSlide(currentIndex);
