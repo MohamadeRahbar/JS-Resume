f3e85d695e4345696cac5817c9b016bc0c708aa0
mmrah <mohamaderahbar@yahoo.com>
2024-12-07 19:29:22 +0330

    navbar fixed

--- cb-ui-temp-01/js/sctipt.js
+++ cb-ui-temp-01/js/sctipt.js
@@ -1,12 +1,17 @@
 /* 
 ============= fixed nav =======================
 */
-const $ = document
+const $ = document;
 const navbar = document.querySelector('.a-navbar');
 $.addEventListener('scroll', function () {
-    if ($.documentElement.scrollTop > 0) {
+    if ($.documentElement.scrollTop > 85 && $.documentElement.scrollTop < 250) {
+        navbar.classList.add('prefixed');
+
+    } else if ($.documentElement.scrollTop >= 250) {
         navbar.classList.add('fixed');
+
     } else {
+        navbar.classList.remove('prefixed');
         navbar.classList.remove('fixed');
     }
 });
