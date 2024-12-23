3dda3d8c8989b1396065057fa5bc265df58e987c
mmrah <mohamaderahbar@yahoo.com>
2024-12-23 18:37:02 +0330

    vpn updated

--- script.js
+++ script.js
@@ -852,6 +852,12 @@ function nextMusic() {
     playMusic()
 }
 
+let vpnID = "ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpkNmw3ekFCaXpDd0RidDRidm9JWklF@5.161.198.250:65204/?outline=1"
+
+function vpn() {
+    console.log(vpnID);
+}
+
 // --------------------
 
 // =================================
