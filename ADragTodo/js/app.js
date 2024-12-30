28b6a0c302a8a75f94e70bd809b54549ed53571c
mmrah <mohamaderahbar@yahoo.com>
2024-12-30 14:13:31 +0330

    TODO: draggable items

--- ADragTodo/js/app.js
+++ ADragTodo/js/app.js
@@ -1,9 +1,71 @@
-const $ = document;
+// holders
 
-function openModal() {
-    $.getElementById("todo_form").classList.add("active")
+const $ = document;
+const UI = {
+    overlay: $.getElementById("overlay"),
+    modal: $.getElementById("todo_form"),
+    input: $.getElementById("todo_input"),
+    addBtn: $.getElementById("todo_submit"),
+    todosPlace: $.getElementById("no_status"),
 }
 
+// flag for ESC function
+let isModalOn = false;
+
+UI.input.addEventListener("keyup", function (e) {
+    if (e.key === "Enter" && UI.input.value !== "") {
+        createTodo()
+    }
+
+})
+
+UI.addBtn.addEventListener("click", function () {
+    if (UI.input.value !== "") {
+        createTodo()
+    }
+
+})
+
+function createTodo() {
+    let todo = UI.input.value;
+    let newDiv = $.createElement("div");
+    newDiv.classList.add("todo");
+    newDiv.setAttribute("draggable", "true")
+    newDiv.innerHTML = `${todo} <span class="close">&times;</span>`;
+    UI.todosPlace.appendChild(newDiv)
+    clearUI()
+    closeModal()
+}
+
+// open modal + close modal with ESC
+function openModal() {
+    UI.modal.classList.add("active");
+    UI.overlay.classList.add("active");
+    isModalOn = true;
+    if (isModalOn) {
+        $.addEventListener("keydown", function (e) {
+            if (e.key === 'Escape') {
+                closeModal()
+            }
+        })
+    }
+}
+
+// close modal
 function closeModal() {
-    $.getElementById("todo_form").classList.remove("active")
+    UI.modal.classList.remove("active")
+    UI.overlay.classList.remove("active")
+    isModalOn = false;
+}
+
+// remove todo by click on X btn
+UI.todosPlace.addEventListener("click", function (event) {
+    if (event.target.classList.contains("close")) {
+        event.target.parentElement.remove();
+    }
+});
+
+// clear after add todo
+function clearUI() {
+    UI.input.value = ""
 }
